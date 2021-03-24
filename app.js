d8263a8ec7643341a8440563f71e601e9f929065
Rachit Sharma <s.rac98@gmail.com>
2021-03-24 19:06:21 +0530

    Level 2

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/usersDB', {useNewUrlParser: true, useUnifiedTopology: true});
+const encrypt = require('mongoose-encryption');
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+const usersSchema = new mongoose.Schema({
+    email: String,
+    password: String 
+});
+
+const secret = "Thisisasecretkey";
+usersSchema.plugin(encrypt, { secret: secret , encryptedFields:['password']});
+
+const User = mongoose.model('User',usersSchema);
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login');
+});
+
+app.get('/register',(req,res)=>{
+    res.render('register');
+});
+
+app.post('/register',(req,res)=>{
+    const email = req.body.username;
+    const pass = req.body.password;
+    const newUser = new User({
+        email: email,
+        password: pass
+    });
+    newUser.save((err)=>{
+        if(err){
+            console.log(err);
+        } else {
+            res.render('secrets');
+        }
+    });
+
+})
+
+app.post('/login',(req,res)=>{
+    const username = req.body.username;
+    const pass = req.body.password;
+    User.findOne({email:username},(err,foundUser)=>{
+        if(err){
+            console.log(err);
+        } else{
+            if(foundUser){
+                if(foundUser.password === pass){
+                    res.render('secrets');
+                }
+            }
+        }
+    })
+})
+
+app.listen(3000,()=>{
+    console.log('Listening on port 3000');
+})
