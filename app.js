d36000b445d496edbbf0777b1ae771f492aa5f0f
Rachit Sharma <s.rac98@gmail.com>
2021-03-26 05:44:01 +0530

    Level 6 OAuth with google

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 const session = require('express-session')
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 const app = express();
 app.set('view engine','ejs');
 app.use(express.urlencoded({extended:true}));
@@ -26,22 +27,55 @@ mongoose.connect('mongodb://localhost:27017/usersDB', {useNewUrlParser: true, us
 mongoose.set('useCreateIndex', true);
 const usersSchema = new mongoose.Schema({
     username: String,
-    password: String 
+    password: String,
+    googleId: String
 });
 
 usersSchema.plugin(passportLocalMongoose);
+usersSchema.plugin(findOrCreate);
 
 const User = mongoose.model('User',usersSchema);
 
 passport.use(User.createStrategy());
 
 // Used only when we are using sessions,(serialize to generate a cookie)
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 app.get('/',(req,res)=>{
     res.render('home');
 })
+
+// This is the callback we added on our google project
+app.get('/auth/google',
+  passport.authenticate("google", { scope: ["profile"] }));
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets page.
+    res.redirect('/secrets');
+  });
 
 app.get('/login',(req,res)=>{
     res.render('login');
@@ -82,7 +116,6 @@ app.post('/login',(req,res)=>{
         username: req.body.username,
         password: req.body.password
     })
-
     req.login(user,(err)=>{
         if(err){
             console.log(err);
